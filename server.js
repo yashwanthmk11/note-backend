0a3ba84f1454de90bd5027abf92b591bed041318
YASHWANTH MK <mkyashwanth3@gmail.com>
2025-06-15 22:53:53 +0530

    Update server.js

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const cors = require('cors');
 const noteRoutes = require('./routes/noteRoutes');
 
 const app = express();
-
+const PORT = process.env.PORT || 5000;
 // Middleware
 app.use(cors());
 app.use(express.json());
